1496d78133192a8c271f4585bd2d4b6aa166846f
Rajat Kumar <rajat.kumar@daffodilsw.com>
2020-06-16 11:50:51 +0530

    interpolation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'youtube';
+  string1 = 'Rajat';
+  array1 = [1, 2, 3, 4];
+  object1 = { 'first_name': 'Rajat', 'last_name': 'Kumar' };
 }
