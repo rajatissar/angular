0a4c83c129c7c80e6350491e3b4618a849e7a7d4
Rajat Kumar <rajat.kumar@daffodilsw.com>
2020-06-17 11:57:36 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { ChildComponent } from './child/child.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'child',
+    component: ChildComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
