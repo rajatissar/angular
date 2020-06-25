c07288f86ab1b258ad91168c22cc15fba3b0ba6c
Rajat Kumar <rajatissar94@gmail.com>
2020-06-25 15:04:57 +0530

    ContentChild added

--- src/app/child2/child2.component.ts
+++ src/app/child2/child2.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ContentChild,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-child2',
   templateUrl: './child2.component.html',
   styleUrls: ['./child2.component.css']
 })
-export class Child2Component implements OnInit {
+
+export class Child2Component implements OnInit, AfterViewInit {
+
+  @ContentChild('contentChild1') contentChild1: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit ->>', this.contentChild1.nativeElement.textContent);
   }
 
 }
