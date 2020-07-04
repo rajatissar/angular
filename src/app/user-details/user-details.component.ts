49937db57bb038cf80c193903b0d57e731c71f3a
Rajat Kumar <rajatissar94@gmail.com>
2020-07-04 17:55:39 +0530

    queryParams added in route

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -9,19 +9,32 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserDetailsComponent implements OnInit {
   id: string;
   name: string;
+  lastName: string;
+  email: string;
 
   constructor(
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    console.log('route data', this.route.snapshot.params);
+    console.log('route data (params)', this.route.snapshot.params);
     this.id = this.route.snapshot.params.id;
     this.name = this.route.snapshot.params.name || 'stranger';
+
     this.route.params
       .subscribe((params: Params) => {
         this.id = params.id;
         this.name = params.name;
+      });
+
+    console.log('route data (queryParams)', this.route.snapshot.queryParams);
+    this.lastName = this.route.snapshot.queryParams.lastName || 'no last name';
+    this.email = this.route.snapshot.queryParams.email || 'no email';
+
+    this.route.queryParams
+      .subscribe((queryParams: Params) => {
+        this.lastName = queryParams.lastName || 'no last name';
+        this.email = queryParams.email || 'no email';
       });
   }
 
